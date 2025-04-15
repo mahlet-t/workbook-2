505c3d321b81febb15f142bde0885cdb28305d1a
mahle <mahletwondwossen71@gmail.com>
2025-04-15 12:39:01 -0500

    add while loop

---
+++ FullNameGenerator/src/main/java/com/pluralsight/HighScoreWins.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class HighScoreWins {
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Please enter a game score in this format: \nTeam1:Team2|Team1score:Team2score ");
+                String game = input.nextLine();
+                String[] parts = game.split(Pattern.quote("|"));
+                if (parts.length != 2) {
+                    throw new Exception();// force jump to catch if format is wrong
+                }
+                String[] teams = parts[0].split(Pattern.quote(":"));
+                String[] score = parts[1].split(Pattern.quote(":"));
+                if (teams.length != 2 || score.length != 2) { // also force error if teams or score are missing
+                    throw new Exception();
+                }
+                int score1 = Integer.parseInt(score[0]);
+                int score2 = Integer.parseInt(score[1]);
+                if (score1 > score2) {
+                    System.out.println("winner: " + teams[0]);
+                } else if (score2 > score1) {
+                    System.out.println("winner: " + teams[1]);
+
+                } else {
+                    System.out.println("Equal! no winners");
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Invalid input please try again");
+            }
+
+
+        }
+    }
+}
