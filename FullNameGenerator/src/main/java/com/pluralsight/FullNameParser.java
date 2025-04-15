a88f99a826e11f42f11f0f5ad158744348540873
mahle <mahletwondwossen71@gmail.com>
2025-04-15 09:36:10 -0500

    Fix capitalization

--- FullNameGenerator/src/main/java/com/pluralsight/FullNameParser.java
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameParser.java
@@ -4,27 +4,41 @@ import java.util.Scanner;
 
 public class FullNameParser {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.println("please enter your name ");
-        String name= input.nextLine().trim();
-        String[] parts=name.split(" ");
-        String firstname=" ";
-        String middleName="none";
-        String  lastName=" ";
-        if (parts.length==2){
-            firstname=parts[0];
-            lastName=parts[1];
-        } else if (parts.length==3) {
-            firstname=parts[0];
-            middleName=parts[1];
-            lastName=parts[2];
-            System.out.println("First name: "+firstname);
-            System.out.println("Middle name: "+middleName);
-            System.out.println("last name: "+lastName);
-        }
-        else {
-            System.out.println("invalid name format try again");
-        }
-    }
+        int tries=0;
+            Scanner input = new Scanner(System.in);
+        while (tries<3){
+            System.out.println("please enter your name ");
+            String name = input.nextLine().trim();//get user input and remove extra spaces
+            String[] parts = name.split(" ");//split the name by space
+            if (parts.length == 2|| parts.length==3) {
+                String firstname =parts[0];
+                String middleName = (parts.length==3)?parts[1] :"none";
+                String lastName = (parts.length==3)?parts[2]:parts[1];
+                // Capitalize first letter and make rest lowercase
+                firstname=firstname.substring(0,1).toUpperCase()+firstname.substring(1).toLowerCase();
+                lastName=lastName.substring(0,1).toUpperCase()+lastName.substring(1).toLowerCase();
+                //only format middle name if it exists
+                if (parts.length==3){
+                    middleName=middleName.substring(0,1).toUpperCase()+middleName.substring(1).toLowerCase();
+                }
+                System.out.println("First name: " + firstname);
+                System.out.println("Middle name: " + middleName);
+                System.out.println("last name: " + lastName);
+                break;// exit loop if valid
+            }
+            else {
+                tries++;// count as a wrong try
+            }
 
+            if (tries <3){
+                System.out.println("Invalid name format try again");
+            }
+            else {
+                System.out.println("Sorry, too many invalid attempts. program will stop.");
+            }
+        }
+
+
+
+    }
 }
